11750ab1ef392d8720e18a7315e881bd8822933a
Flavio Coutinho <fegemo@gmail.com>
2016-05-19 16:10:39 -0300

    Adds comments to routes/people.js to guide the exercise

--- routes/people.js
+++ routes/people.js
@@ -8,10 +8,23 @@ router.get('/', function(req, res, next) {
 
   db.query({
     sql: 'SELECT * FROM person LEFT OUTER JOIN zombie ON eatenBy = zombie.id',
+    // nestTables resolve conflitos de haver campos com mesmo nome nas tabelas
+    // nas quais fizemos JOIN (neste caso, `person` e `zombie`).
+    // descrição: https://github.com/felixge/node-mysql#joins-with-overlapping-column-names
     nestTables: true
     }, function(err, rows) {
-      if (err) res.status(500).send('Problema ao recuperar pessoas.');
+      if (err) {
+        res.status(500)
+          .send('Problema ao recuperar pessoas. Descrição: ' + err);
+      }
 
+      // renderiza a view de listagem de pessoas, passando como contexto
+      // de dados:
+      // - people: com um array de `person`s do banco de dados
+      // - success: com uma mensagem de sucesso, caso ela exista
+      //   - por exemplo, assim que uma pessoa é excluída, uma mensagem de
+      //     sucesso pode ser mostrada
+      // - error: idem para mensagem de erro
       res.render('listPeople', {
         people: rows,
         success: req.flash('success'),
@@ -27,7 +40,9 @@ router.put('/eaten/', function(req, res) {
            'SET alive = false, eatenBy = ' + db.escape(req.body.zombie) + ' ' +
            'WHERE id = ' + db.escape(req.body.person),
     function(err, result) {
-      if (result.affectedRows !== 1) {
+      if (err) {
+        req.flash('error', 'Erro desconhecido. Descrição: ' + err);
+      } else if (result.affectedRows !== 1) {
         req.flash('error', 'Nao ha pessoa para ser comida');
       } else {
         req.flash('success', 'A pessoa foi inteiramente (nao apenas cerebro) engolida.');
@@ -44,9 +59,20 @@ router.get('/new/', function(req, res) {
 
 
 /* POST registra uma nova pessoa */
-// IMPLEMENTAR AQUI
+// Exercício 1: IMPLEMENTAR AQUI
+// Dentro da callback de tratamento da rota:
+//   1. Fazer a query de INSERT no banco
+//   2. Redirecionar para a rota de listagem de pessoas
+//      - Em caso de sucesso do INSERT, colocar uma mensagem feliz
+//      - Em caso de erro do INSERT, colocar mensagem vermelhinha
+
 
 /* DELETE uma pessoa */
-// IMPLEMENTAR AQUI
+// Exercício 2: IMPLEMENTAR AQUI
+// Dentro da callback de tratamento da rota:
+//   1. Fazer a query de DELETE no banco
+//   2. Redirecionar para a rota de listagem de pessoas
+//      - Em caso de sucesso do INSERT, colocar uma mensagem feliz
+//      - Em caso de erro do INSERT, colocar mensagem vermelhinha
 
 module.exports = router;
